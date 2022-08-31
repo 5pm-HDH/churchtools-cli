045463aa30c10e2a4e7b3b99622b0406bc66db47
DumbergerL <lukas.dumberger@gmail.com>
2022-08-31 13:51:28 +0200

    style(format): remove unused import

--- src/Commands/ReportCommands/ReportCommand.php
+++ src/Commands/ReportCommands/ReportCommand.php
@@ -4,7 +4,6 @@ namespace CTExport\Commands\ReportCommands;
 
 use CTExport\Commands\AbstractCommand;
 use CTExport\Commands\ReportCommands\ReportBuilders\ReportBuilder;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
