f5c5bc6522190b24b068ae5aea91f992ab590685
DumbergerL <lukas.dumberger@gmail.com>
2022-12-08 15:48:26 +0100

    feat(export-round-birthdays): export round birthdays

--- src/Commands/Traits/LoadPersons.php
+++ src/Commands/Traits/LoadPersons.php
@@ -11,8 +11,6 @@ trait LoadPersons
 {
     protected function loadBirthdayPerson(string $birthdayBefore, array $statusIds): array
     {
-        CTLog::enableConsoleLog();
-        CTLog::enableFileLog();
         return PersonRequest::where('status_ids', $statusIds)
             ->where('birthday_before', $birthdayBefore)
             ->get();
